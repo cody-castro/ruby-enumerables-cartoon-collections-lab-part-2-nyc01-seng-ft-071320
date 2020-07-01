6089f8ce2cffe2e3cbcaa14eaa07dd8224458a89
Cody C <codyicastro@gmail.com>
2020-07-01 15:34:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,4 +31,5 @@ def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find_all do |element|
     element == valid_calls
+  end
 end

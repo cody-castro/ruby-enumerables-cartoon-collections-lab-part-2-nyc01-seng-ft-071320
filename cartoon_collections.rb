dd2533cd07eb015b85aaaaf91cb98e61a88c42cf
Cody C <codyicastro@gmail.com>
2020-07-01 15:19:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ end
   # Return a new array of the results
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map do |element|
- return element.capitalize << "!"
+ element.capitalize << "!"
   end
 end
 

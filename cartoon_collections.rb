a762e71d1d34eddc653e866d9682d96b9287c1fe
Cody C <codyicastro@gmail.com>
2020-07-01 15:43:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,7 +29,6 @@ end
   # Return the first valid call found, or return nil if no valid calls are found
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find do |element|
-    element == valid_calls.include? (valid_calls)
+  planeteer_calls.find {|string| valid_calls.include? ( )} 
   end
 end
